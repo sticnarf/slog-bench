4a275130fae083620aa4b2a5dfdd1099be98021b
Yilin Chen <sticnarf@gmail.com>
2021-08-26 01:43:11 +0800

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{fs::OpenOptions, thread};
 
-use slog::*;
+use slog::{slog_o, Drain};
 use slog_async::Async;
+use slog_global::info;
 
 fn main() {
     let log_path = "/tmp/mylog.log";
@@ -13,20 +14,19 @@ fn main() {
         .unwrap();
     let decorator = slog_term::PlainDecorator::new(file);
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-    let (async_log, _guard) = Async::new(drain)
+    let async_log = Async::new(drain)
         .chan_size(10240)
         .overflow_strategy(slog_async::OverflowStrategy::Block)
         .thread_name("slogger".to_string())
-        .build_with_guard();
+        .build();
     let drain = async_log.fuse();
-    let log = slog::Logger::root(drain, o!());
+    let logger = slog::Logger::root(drain, slog_o!());
+    slog_global::set_global(logger);
 
     let mut ths = Vec::new();
     for _ in 0..4 {
-        let log = log.clone();
         let th = thread::spawn(move || loop {
-            info!(log, "hello!"; "where" => "right here");
+            info!("hello!"; "where" => "right here");
         });
         ths.push(th);
     }
