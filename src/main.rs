11761e50acef8d91e04a310ec74b2a98933e1978
Yilin Chen <sticnarf@gmail.com>
2021-08-26 13:23:04 +0800

    set same version with tikv

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::{fs::OpenOptions, io::BufWriter, thread};
+use std::{
+    env,
+    fs::{self, OpenOptions},
+    io::BufWriter,
+    path::Path,
+    thread,
+    time::{Duration, Instant},
+};
 
-use slog::{slog_o, Drain};
+use slog::{slog_o, Drain, Level};
 use slog_async::Async;
-use slog_global::info;
+use slog_global::*;
 
 fn main() {
-    let log_path = "/tmp/mylog.log";
+    let log_path = env::args().skip(1).next().unwrap();
+    let log_path = Path::new(&log_path);
     let file = OpenOptions::new()
         .create(true)
         .write(true)
@@ -19,18 +27,29 @@ fn main() {
         .overflow_strategy(slog_async::OverflowStrategy::Block)
         .thread_name("slogger".to_string())
         .build();
-    let drain = async_log.fuse();
+    let drain = async_log.filter_level(Level::Info).fuse();
     let logger = slog::Logger::root(drain, slog_o!());
     slog_global::set_global(logger);
 
     let mut ths = Vec::new();
+    let start = Instant::now();
+    let time = Duration::from_secs(10);
     for _ in 0..4 {
-        let th = thread::spawn(move || loop {
-            info!("hello!"; "where" => "right here");
+        let th = thread::spawn(move || {
+            while Instant::now() < start + time {
+                for _ in 0..100 {
+                    info!("hello!"; "where" => "right here");
+                }
+            }
         });
         ths.push(th);
     }
     for th in ths {
         th.join().unwrap();
     }
+    let size = fs::metadata(log_path).unwrap().len();
+    println!(
+        "{:.2} MiB/s",
+        size as f64 / (1 << 20) as f64 / start.elapsed().as_secs_f64()
+    );
 }
